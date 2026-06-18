9868e3fa8cea465d5d68e3cd495427be8677a4a5
Alberto Santes <albertosantes21@hotmail.com>
2026-06-17 22:15:52 -0600

    Agrego nuevos cambios

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 api_key = "sk_test_4eC39HqLLyoP9a0n4mT7Dc"
+
+private_key = """-----BEGIN RSA PRIVATE KEY-----
+MIIEpQIBAAKCAQEA1234567890abcdefSIMULADAfedcba0987654321zyxwvu
+TSRQPONMLKJIHGFEDCBA9876543210ZYXWVUTSRQPONMLKJIHGFEDCBA1234567
+-----END RSA PRIVATE KEY-----"""
+
+db_config = {
+    "host": "localhost",
+    "port": 5432,
+    "database": "curso_git_db",
+    "user": "usuario_demo",
+    "password": "clave_simulada123",
+}
